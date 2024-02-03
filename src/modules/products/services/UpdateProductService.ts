5eacf82adec812a49649f296a4dbbc20e1cc317f
virgilio-vpr <virgilio.vpr@gmail.com>
2024-02-03 17:37:01 -0300

    Session 13 - Implementing cache with Redis - class 107 - Invalidating the cache

--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import ProductRepository from '../typeorm/repositories/ProductsRepository';
 import Product from '../typeorm/entities/Product';
 import AppError from '@shared/errors/AppError';
+import RedisCache from '@shared/cache/RedisCache';
 
 interface IRequest {
   id: string;
@@ -30,6 +31,10 @@ class UpdateProductService {
     if (productExists && name !== product.name) {
       throw new AppError('There is already one product winth this name');
     }
+
+    const redisCache = new RedisCache();
+
+    await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
     product.name = name;
     product.price = price;
